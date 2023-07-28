5c153cabdf35dd6f7c0028738d693ee7344c0362
NaveenFW <136461239+fwnaveen14@users.noreply.github.com>
2023-07-28 11:24:39 +0530

    Add files via upload

---
+++ Lab04Q2/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04Q2
+{
+    internal class FindValue
+    {
+        public double findArea(double x)
+        {
+
+            
+            
+            double area = 3.14 * x * x;
+            
+
+            return area;
+        }
+
+        public double findCircumference(double y)
+        {
+            double cir = 2 * 3.14 * y;
+            ;   
+            return cir;
+        }
+    }
+}
---
+++ Lab04Q2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the radius:");
+            double radius = double.Parse(Console.ReadLine());
+            FindValue cal = new FindValue();          
+            double x = cal.findArea(radius);
+            Console.WriteLine("Area = "+ x);
+
+            double y = cal.findCircumference(radius);
+            Console.WriteLine("Circumference = "+  y);
+            Console.ReadLine();
+        }
+    }
+}
